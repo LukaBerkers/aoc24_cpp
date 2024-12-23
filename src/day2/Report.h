91f23c8071690cc81e4893b3bf459938e9066487
Luka <luka@berkers.dev>
2024-12-23 13:19:55 +0100

    feat: add custom formatter for `Report` class
    Introduce a `fmt::formatter` specialization for the `Report` class. This provides formatted string representations of the `Report` object, improving logging and debugging capabilities.

--- src/day2/Report.h
+++ src/day2/Report.h
@@ -1,6 +1,8 @@
 #ifndef AOC24_CPP_SRC_DAY2_REPORT_H_
 #define AOC24_CPP_SRC_DAY2_REPORT_H_
 
+#include <spdlog/formatter.h>
+
 #include <cstddef>
 #include <initializer_list>
 #include <string>
@@ -26,5 +28,27 @@ class Report final {
 };
 
 }  // namespace aoc24::day2
+
+namespace fmt {
+
+using namespace aoc24::day2;
+
+template <>
+struct formatter<Report> : formatter<std::string> {
+    format_context::iterator format(const Report& report, format_context& ctx) const {
+        const auto& levels{report.levels()};
+        std::string levels_str{'['};
+
+        for (std::size_t i{0}; i < levels.size() - 1; ++i) {
+            levels_str += std::to_string(levels[i]) + ", ";
+        }
+
+        levels_str += std::to_string(levels.back()) + ']';
+        const std::string output{"Report { levels: " + levels_str + " }"};
+        return formatter<std::string>::format(output, ctx);
+    }
+};
+
+}  // namespace fmt
 
 #endif  // AOC24_CPP_SRC_DAY2_REPORT_H_
