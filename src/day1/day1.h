c1b4febfd44e0adc1666856779fb26bf65fc0129
Luka <luka@berkers.dev>
2024-12-24 16:13:31 +0100

    chore: add GPLv3 license file
    This commit adds the GNU General Public License version 3 to the repository. The license ensures the project is open source and defines permissions for use, modification, and distribution.

--- src/day1/day1.h
+++ src/day1/day1.h
@@ -1,3 +1,19 @@
+// This file is part of my solutions for Advent of Code 2024.
+// Copyright (C) 2024  Luka Berkers
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef AOC24_CPP_SRC_DAY1_DAY1_H_
 #define AOC24_CPP_SRC_DAY1_DAY1_H_
 
